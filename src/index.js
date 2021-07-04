77c55fdb9df37ece6e2848c4e5471e6a918af5b3
Serhii Chekalov <78024336+Serhii-Chekalov@users.noreply.github.com>
2021-07-04 23:05:02 +0300

    changes2

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import './sass/main.scss';
 
 //create markup
 import buildMenu from './js/build-menu'
+
+//Input Change
+import inputchange from './js/input-change'
---
+++ src/js/input-change.js
@@ -0,0 +1,25 @@
+import refs from '../js/refs';
+
+console.log(refs.body);
+console.log(refs.inputCheckbox);
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+refs.body.classList.add(localStorage.getItem('theme')?localStorage.getItem('theme'):Theme.LIGHT)
+
+const checkedTheme = event => {
+    if (event.target.checked){
+        localStorage.setItem('theme', Theme.DARK);
+        refs.body.classList.replace(Theme.LIGHT,Theme.DARK);
+    return;
+    }
+
+    localStorage.setItem('theme', Theme.LIGHT);
+    refs.body.classList.add(Theme.LIGHT);
+    refs.body.classList.remove(Theme.DARK);
+};
+
+refs.inputCheckbox.addEventListener('change', checkedTheme);
